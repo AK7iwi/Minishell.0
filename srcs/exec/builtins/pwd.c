fda4cf20f98e3d2e5600ce07af4af3a3c68e9e1c
AK7iwi <ATZLTS@proton.me>
2024-10-01 12:55:48 +0200

    Exec/builtins: fix pwd

--- srcs/exec/builtins/pwd.c
+++ srcs/exec/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 11:28:19 by diguler           #+#    #+#             */
-/*   Updated: 2024/09/29 20:40:35 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:54:25 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,5 @@ void	pwd(void)
 		printf("pwd error"); //msg error
 	else
 		printf("%s\n", cwd);
+	free(cwd);
 }
--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 11:38:35 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/10/01 12:45:59 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:52:02 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ static bool exec_cmd(t_data *data, t_cmd *cmd)
 {	
 	if (is_builtins(data, cmd->args))
 		return (EXIT_SUCCESS);
+	//is_cmd
 	
 	return (EXIT_FAILURE);
 }
