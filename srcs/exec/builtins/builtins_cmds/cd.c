3d8572578dad863095d825f43733f0e90144c827
AK7iwi <ATZLTS@proton.me>
2024-10-06 16:58:11 +0200

    Exec/builtins: cd fix bug

--- srcs/exec/builtins/builtins_cmds/cd.c
+++ srcs/exec/builtins/builtins_cmds/cd.c
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:08:47 by diguler           #+#    #+#             */
-/*   Updated: 2024/10/06 16:48:35 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/10/06 16:57:50 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,30 @@
 
 static bool update_env(t_env **env, char *old_cwd)
 {
+	t_env *current;
 	char *cwd;
 	
+	current = (*env);
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 		return (EXIT_FAILURE);
 	
-	while (*env)
+	while (current)
 	{
-		if (ft_strncmp((*env)->str, "PWD=", 4) == 0)
+		if (ft_strncmp(current->str, "PWD=", 4) == 0)
 		{
-			free((*env)->str);
-			(*env)->str = ft_strjoin("PWD=", cwd);
+			free(current->str);
+			current->str = ft_strjoin("PWD=", cwd);
 		}
-		else if (ft_strncmp((*env)->str, "OLDPWD=", 7) == 0)
+		else if (ft_strncmp(current->str, "OLDPWD=", 7) == 0)
 		{
-			free((*env)->str);
-			(*env)->str = ft_strjoin("OLDPWD=", old_cwd);
-		}
-		
-		(*env) = (*env)->next;
+			free(current->str);
+			current->str = ft_strjoin("OLDPWD=", old_cwd);
+		}	
+		current = current->next;
 	}
 	
 	free(cwd);
-	
 	return (EXIT_SUCCESS);
 }
 bool	cd(t_data *data, char **args)
@@ -47,17 +47,18 @@ bool	cd(t_data *data, char **args)
 	
 	if (args[1] && args[2])
 		return (data->error.exec_errors |= ERROR_CD1, false);
-	
-	old_cwd = getcwd(NULL, 0);
-	if (!old_cwd)
-		return (data->error.exec_errors |= ERROR_CD2, false);
-	
+	//protect if (!env)
+	//fct dir 
     if (!args[1] || ft_strncmp(args[1], "~", 1) == 0)
         dir = getenv("HOME");
     else if (strcmp(args[1], "-") == 0)
         dir = getenv("OLDPWD");
 	else
         dir = args[1];
+
+	old_cwd = getcwd(NULL, 0);
+	if (!old_cwd)
+		return (data->error.exec_errors |= ERROR_CD2, false);
 	
     if (!dir || chdir(dir) != 0)
 		return (data->error.exec_errors |= ERROR_CD2, false);
@@ -65,7 +66,6 @@ bool	cd(t_data *data, char **args)
 		return (data->error.exec_errors |= ERROR_CD2, false);
 	
 	free(old_cwd);
-	
     return (true);
 }
 
